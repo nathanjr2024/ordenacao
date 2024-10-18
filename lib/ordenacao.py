bedde5cbb2972ad2e4973d4277b2f8a5f7027830
beafragoso <beatrizfragoli@gmail.com>
2024-10-17 22:33:59 -0300

    Update ordenacao.py

--- lib/ordenacao.py
+++ lib/ordenacao.py
@@ -1,7 +1,11 @@
 def bubble_sort(lista):
     x = len(lista)
     for i in range(x):
+        sorteado = False
         for j in range(0, x-i-1):
             if lista[j] > lista[j + 1]:
-                lista[j], lista[j + 1] = lista[j + 1], lista[j]            
-    return lista
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+                sorteado = True
+        if not sorteado:
+            break
+            
