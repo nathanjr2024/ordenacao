2dcb73b5d78df50ff3735fbbe3047e073780e854
Leo <160294061+ptgz@users.noreply.github.com>
2024-10-17 22:03:08 -0300

    Implementação do Bubble Sort

--- lib/ordenacao.py
+++ lib/ordenacao.py
@@ -1,3 +1,7 @@
 
 def bubble_sort(lista: list) -> None:
-    pass
+    for i in range(len(list)):
+    for j in range(len(list)-1):
+      if list[j] > list[j+1]:
+        list[j], list[j+1] = list[j+1], list[j]
+    return list
